bcf373751f9fbbc45b567e35371a727ab7d8afe7
soumya210611 <soumya210611@gmail.com>
2023-04-26 18:32:25 +0530

    student details added

---
+++ StudentDetails2.java
@@ -0,0 +1,17 @@
+package randomPograms;
+
+public class StudentDetails2 {
+	public void address() {
+		String addressString="Kerala";
+		System.out.println(addressString);
+	}
+
+	public static void main(String[] args) {
+		Student1 student=new Student1();
+		student.details();
+		StudentDetails2 studentDetails2=new StudentDetails2();
+		studentDetails2.address();
+
+	}
+
+}
