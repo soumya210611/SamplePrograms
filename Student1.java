e88f562913f271aabb20e4f5baa39b528f75df97
soumya210611 <soumya210611@gmail.com>
2023-04-26 18:30:44 +0530

    student1

---
+++ Student1.java
@@ -0,0 +1,11 @@
+package randomPograms;
+
+public class Student1 {
+	public void details() {
+		String name="Soumya";
+		int rollno=21;	
+		System.out.println(name);
+		System.out.println(rollno);
+	}
+
+}
