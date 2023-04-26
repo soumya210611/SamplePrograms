a2e1db93abee114c4321d801ddbbfe80453f6bed
soumya210611 <soumya210611@gmail.com>
2023-04-26 18:22:06 +0530

    add offseason

---
+++ OffSeason.java
@@ -0,0 +1,13 @@
+package randomPograms;
+
+public class OffSeason {
+	public OffSeason() {
+		System.out.println("constructor 1");
+	}
+public void cloths(float price) {
+	
+	double off= (price-(price*0.15));
+	System.out.println(off);
+	
+}
+}
